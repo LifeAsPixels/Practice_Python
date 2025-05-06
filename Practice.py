ed314748a5960f5f9872c5d34c8a3bc9d7756456
L1f34sP1x3ls <11801756+L1f34sP1x3ls@users.noreply.github.com>
2025-05-06 10:00:58 -0400

    Palindrome Numbers
    added method isPalindromeNumber

--- Practice.py
+++ Practice.py
@@ -5,5 +5,9 @@ def main():
 
     Instance = Problems.Problems()
 
-    print(Instance.Fibonacci())
-    print(Instance.Fizz_Buzz())
+    Instance.Fibonacci(8)
+    Instance.Fizz_Buzz(15)
+    Instance.isPalindromeNumber(150)
+
+if __name__ == "__main__":
+    main()
--- Practice/Problems.py
+++ Practice/Problems.py
@@ -1,34 +1,34 @@
-# import inspect 
+import inspect 
 
-# class Console_Utility:
+class Console_Utility:
     
-#     def Header(self, title = ""):
+    def Header(self, title = ""):
 
-#         if title != "":
-#             print()
-#             print()
-#             print(f'{'<'.ljust(101, '=')}>')
-#             print(f"<{title.ljust(60, ' ').rjust(65, ' ').ljust(80, '-').rjust(100, '-')}>")
-#             print(f'{'<'.ljust(101, '_')}>')
-#         else:
-#             print()
-#             print()
-#             caller_frame = inspect.stack()[1]
-#             print(f'{'<'.ljust(101, '=')}>')
-#             print(f"<{caller_frame.function.ljust(60, ' ').rjust(65, ' ').ljust(80, '-').rjust(100, '-')}>")
-#             print(f'{'<'.ljust(101, '_')}>')
+        if title != "":
+            print()
+            print()
+            print(f'{'<'.ljust(101, '=')}>')
+            print(f"<{title.ljust(60, ' ').rjust(65, ' ').ljust(80, '-').rjust(100, '-')}>")
+            print(f'{'<'.ljust(101, '_')}>')
+        else:
+            print()
+            print()
+            caller_frame = inspect.stack()[1]
+            print(f'{'<'.ljust(101, '=')}>')
+            print(f"<{caller_frame.function.ljust(60, ' ').rjust(65, ' ').ljust(80, '-').rjust(100, '-')}>")
+            print(f'{'<'.ljust(101, '_')}>')
             
  
 class Problems:
 
-    # Util = Console_Utility()
+    Util = Console_Utility()
 
     def Fibonacci(self, number):
         '''fibonacci sequence has two starting seeds 0 and 1 or 1 and 1
         each number is given a position like an index
         the most recent two numbers are added together to make the next number '''
 
-        # self.Util.Header()
+        self.Util.Header()
         
         # instantiate the list with zeros at the size of the sequence n + 1
         fib = [0] * number
@@ -45,12 +45,31 @@ class Problems:
         print buzz if a number is divisible by 5
         print fizzbuzz if a number is divisible by 3 and 5'''
 
-        for index in number + 1:
-            match index:
-                case index if index % 3 == 0:
-                    return "fizz\n"
-                case index if index % 5 == 0:
-                    return "buzz\n"
-                case _:
-                    return "..."
+        self.Util.Header()
 
+        for index in range(0, number + 1):
+            match index:
+                case index if (index % 3 == 0) and (index % 5 == 0):
+                    print("fizzbuzz")
+                case index if index % 3 == 0:
+                    print("fizz")
+                case index if index % 5 == 0:
+                    print("buzz")
+                case _:
+                    print("...")
+
+
+    def isPalindromeNumber(self, number: int):
+        '''take in a max number
+        iterate over every number through that number
+        check if they are palindromes'''
+        self.Util.Header()
+
+        palindrome = []
+        for item in range(0, number + 1):
+
+            if str(item)[::-1] == str(item):
+                palindrome.append(item)
+        
+        print(palindrome)
+                
